3e7242a9b8eab24820a4adf5f3438d59bd5c850f
oreiscool <adetanoreoluwa1@gmail.com>
2026-06-28 19:06:46 +0100

    feat(calc_button): implement reusable calculator button widget

---
+++ lib/widgets/calc_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CalcButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onTap;
+  final Color color;
+  final Color textColor;
+  final bool isCompact;
+
+  const CalcButton({
+    super.key,
+    required this.label,
+    required this.onTap,
+    this.color = Colors.grey,
+    this.textColor = Colors.white,
+    this.isCompact = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          shape: isCompact ? BoxShape.rectangle : BoxShape.circle,
+          color: color,
+        ),
+
+        child: Center(
+          child: Text(label, style: TextStyle(color: textColor)),
+        ),
+      ),
+    );
+  }
+}
